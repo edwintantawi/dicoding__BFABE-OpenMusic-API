910c63049651fc4279f7b8fafd5acf5d2724019f
edwintantawi <edwintantawi@gmail.com>
2021-10-20 12:04:14 +0700

    🔧 add AUTH_STRATEGY config

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,32 @@
 require('dotenv').config();
 
+const getEnv = (key) => {
+  const env = process.env[key];
+  if (!env) throw new Error(`Couldn't find enviroment variabel: ${key}`);
+  return env;
+};
+
 const SERVER_CONFIG = {
-  PORT: process.env.PORT,
-  HOST: process.env.HOST,
+  PORT: getEnv('PORT'),
+  HOST: getEnv('HOST'),
 };
 
 const JWT_CONFIG = {
-  ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
+  ACCESS_TOKEN_KEY: getEnv('ACCESS_TOKEN_KEY'),
+  REFRESH_TOKEN_KEY: getEnv('REFRESH_TOKEN_KEY'),
+  AUTH_STRATEGY: {
+    keys: getEnv('ACCESS_TOKEN_KEY'),
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: getEnv('ACCESS_TOKEN_AGE'),
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: { id: artifacts.decoded.payload.id },
+    }),
+  },
 };
 
-module.exports = { SERVER_CONFIG, JWT_CONFIG };
+module.exports = { getEnv, SERVER_CONFIG, JWT_CONFIG };
