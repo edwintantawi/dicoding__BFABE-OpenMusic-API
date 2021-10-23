8d6d6681e5fef35c31813e0665483a037bb6f893
edwintantawi <edwintantawi@gmail.com>
2021-10-23 12:28:23 +0700

    🚀 add table collaborations miragtion

---
+++ migrations/1634960951040_create-table-collaborations.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('collaborations', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  // collaborations.playlist_id -> playlist.id
+  pgm.createConstraint(
+    'collaborations',
+    'fk_collaborations.playlist_id_playlists.id',
+    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE'
+  );
+
+  // collaborations.user_id -> users.id
+  pgm.createConstraint(
+    'collaborations',
+    'fk_collaborations.user_id_users.id',
+    'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE'
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint(
+    'collaborations',
+    'fk_collaborations.playlist_id_playlists.id'
+  );
+  pgm.dropConstraint('collaborations', 'fk_collaborations.user_id_users.id');
+  pgm.dropTable('collaborations');
+};
